a0b4eaaaab02d512d41bf1e0250b735aec6ffd59
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2017-05-24 16:58:38 +0100

    update feature

--- Codes/FeatureSelection.py
+++ Codes/FeatureSelection.py
@@ -3,15 +3,22 @@ from sklearn.feature_selection import chi2
 from sklearn.feature_selection import SelectFromModel
 from sklearn.svm import LinearSVC
 
-class FeatureSelection():
+class FeatureSelection(object):
 	def __init__(self, data, target):
 		self.data = data
 		self.target = target 
 
-	def selection_from_model_l1():
+	def selection_from_model_l1(self):
 		lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(self.data, self.target)
 		model = SelectFromModel(lsvc, prefit=True)
 		return model.transform(self.data)
-
-	def selection_chi2(number_node):
-		 return SelectKBest(chi2, k=number_node).fit_transform(self.data, self.target)
+	
+	def selection_chi2(self, number_node):
+		""" Return a new dataset with the number of node 
+			It's for classification
+		"""
+		return SelectKBest(chi2, k=number_node).fit_transform(self.data, self.target)
+	
+	def selection_classif(self):
+		return f_classif(self.data, self.target)
+		

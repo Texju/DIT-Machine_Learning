57bf365f809d5c19414e39c53123bf2d0c5f4ee3
Jean Thevenet <jean.thevenet@on.nokia.com>
2017-05-25 14:24:19 +0100

    Added naive (always true) predictions

--- Codes/validation.py
+++ Codes/validation.py
@@ -1,4 +1,5 @@
 from sklearn import metrics
+import sys
 
 class MLValidation:
     """This class calculates the preciseness of our tree against a set of data"""
@@ -9,25 +10,24 @@ class MLValidation:
         self.__predictions = []
         
     def test(self, data):
-        """treeForConversion = MLTree()
-        
-        treeForConversion.ignored_features = self.__tree.ignored_features
-        treeForConversion.setTrainingData(data, True)
-        treeForConversion.prepareData(dataType)
-        """
-        
+        """ Testing the model """
         # Train our model
         instances_train, target_train = self.__tree.prepareData(data.Training)
         self.__tree.Tree.fit(instances_train, target_train)
         
         # Test the model
         instances_test, target_test = self.__tree.prepareData(data.Testing)
-        
+
         self.__targets = target_test
-        
+
         #Use the model to make predictions for the test set queries
         self.__predictions = self.__tree.Tree.predict(instances_test)
 
+    def testNaiveAlwaysYes(self, data):
+        """ Test our targets against a matrix that always return - 50000"""
+        self.test(data)
+        self.__predictions[:] = " - 50000."
+            
     def confusionMatrix(self):
         if len(self.__predictions) != 0:
             return metrics.confusion_matrix(self.__targets, self.__predictions)
